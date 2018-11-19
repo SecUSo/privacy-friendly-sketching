3396391f4aba12402561e0572ee38016654cf9bc
Enes13 <enese31@gmail.com>
2018-11-19 16:53:37 +0100

    Added KeyGen Activity for first start

---
+++ app/src/main/java/org/secuso/privacyfriendlyexample/activities/KeyGenActivity.java
@@ -0,0 +1,12 @@
+package org.secuso.privacyfriendlyexample.activities.helper;
+
+/**
+ * Created by enyone on 11/19/18.
+ */
+
+public class KeyGenActivity extends BaseActivity {
+    @Override
+    protected int getNavigationDrawerID() {
+        return 0;
+    }
+}
