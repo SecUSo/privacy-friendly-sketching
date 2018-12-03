d067ff5b98642dbf3214893887fc48f83ad0f981
Enes13 <enese31@gmail.com>
2018-12-03 19:41:21 +0100

    Added beginning of key generation

--- app/src/main/java/org/secuso/privacyfriendlyexample/activities/KeyGenActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyexample/activities/KeyGenActivity.java
@@ -1,12 +1,32 @@
 package org.secuso.privacyfriendlyexample.activities;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.security.KeyPairGeneratorSpec;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.util.Log;
+import android.widget.TextView;
 
 import org.secuso.privacyfriendlyexample.activities.helper.BaseActivity;
 import org.secuso.privacyfriendlyexample.R;
+import org.w3c.dom.Text;
 
+import java.math.BigInteger;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.util.Calendar;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.security.auth.x500.X500Principal;
 
 /**
  * Created by enyone on 11/19/18.
@@ -18,6 +38,16 @@ public class KeyGenActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keygen);
+
+        keyGenClass keyGenClass = new keyGenClass();
+        try {
+            keyGenClass.doInBackground(new URL("http://bla.de"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Intent i = new Intent(KeyGenActivity.this, MainActivity.class);
+        startActivity(i);
+
     }
 
     @Override
@@ -33,16 +63,62 @@ public class KeyGenActivity extends BaseActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            TextView tv = findViewById(R.id.keyGenInfoText);
+            tv.setText("0%");
         }
 
 
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
+
+
         }
 
         @Override
         protected Long doInBackground(URL... urls) {
+            Log.i("KEYGEN_ACTIVITY", "generating keys for later use..");
+
+            //Generates a passphrase for the encrypted ROOM DB
+            SecureRandom random = new SecureRandom();
+            byte[] passphrase = random.generateSeed(20);
+
+            //Generates a key to save the passphrase via Android keystore provider
+            //Asymmetric Encryption which is supported on API 21
+            KeyPairGenerator kpg = null;
+            try {
+                kpg = KeyPairGenerator.getInstance("RSA");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            if (kpg == null) {
+                Log.i("KEYGEN_ACTIVITY", "keypairgenerator is null");
+                System.exit(0);
+            }
+            kpg.initialize(2048);
+            KeyPair pair = kpg.generateKeyPair();
+//            try {
+//                kpg = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore");
+//                Calendar start = Calendar.getInstance();
+//                Calendar end = Calendar.getInstance();
+//                end.add(Calendar.YEAR, 30);
+//                KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(KeyGenActivity.this.getApplicationContext()).setAlias(getResources().getString(R.string.key_alias)).setSubject(new X500Principal("CN=" + getResources().getString(R.string.key_alias))).setSerialNumber((BigInteger.ONE)).setStartDate(start.getTime()).setEndDate(end.getTime())
+//                        .build();
+//                kpg.initialize(spec);
+//            } catch (NoSuchAlgorithmException e) {
+//                e.printStackTrace();
+//            } catch (NoSuchProviderException e) {
+//                e.printStackTrace();
+//            } catch (InvalidAlgorithmParameterException e) {
+//                e.printStackTrace();
+//            }
+//            if (kpg == null) {
+//                Log.i("KEYGEN_ACTIVITY", "keypairgenerator is null");
+//                System.exit(0);
+//            }
+//            kpg.generateKeyPair();
+
+
             return new Long(0);
         }
     }
